1ae74de99a5ac7217d6a84fc1a8ea1554009734e
buingocson203 <sondepdai03@gmail.com>
2023-05-28 08:24:16 +0700

    init project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log('hello word');
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+const app = document.querySelector("#app");
+import { render, router } from "./utilities";
+
+router.on('/', () => render(HomePage, app));
+router.on('/about', () => render(AboutPage, app));
+
+import HomePage from "./pages/Homepage";
+import AboutPage from "./pages/AboutPage";
+
+
+router.resolve();
+
+
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,1 +1,8 @@
-export const myName = "Bui Ngoc Son";
+import Navigo from 'navigo';
+// const router = new Navigo("/", { linksSelector: "a", hash: true})
+const render = (content, target) => {
+    target.innerHTML = content();
+};
+
+export { render, router };
+ 
